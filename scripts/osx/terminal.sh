0a4daf26df2b1737c0365d023000da353f5c4a40
Hargobind S. Khalsa <khalsah@gmail.com>
2016-03-27 15:37:51 -0600

    Update Terminal.app theme

--- scripts/osx/terminal.sh
+++ scripts/osx/terminal.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "Installing Solarized Light Theme and Settings"
 
-open "$(dirname $0)/data/Solarized Light.terminal"
+open "$(dirname $0)/data/Base16 Solarized Light.terminal"
 sleep 1
 
 echo "Configuring Solarized Light as Default"
 osascript <<EOF
 tell application "Terminal"
-  set default settings to settings set "Solarized Light"
+  set default settings to settings set "Base16 Solarized Light"
 end tell
 EOF
